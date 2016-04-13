185b6eba51f4a93bf0c4200e1a3b496448838581
Theo85 <teodor.krawiec@gmail.com>
2016-04-13 17:06:05 +0200

    Menu work in progress

--- app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
+++ app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
@@ -3,6 +3,8 @@ package com.gmail.krawiec.teodor.linkapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -11,6 +13,18 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+*/
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     public void openUnitsActivity(View view) {
