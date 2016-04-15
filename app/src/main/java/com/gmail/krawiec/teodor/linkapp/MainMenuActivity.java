0a7b29a1bf130801b5e87e333b6800dfe99bad8e
Theo85 <teodor.krawiec@gmail.com>
2016-04-15 13:46:28 +0200

    Menu testing - knowledge consolidation

--- app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
+++ app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
@@ -1,11 +1,13 @@
 package com.gmail.krawiec.teodor.linkapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -14,13 +16,13 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
-/*
+
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
-*/
+
     @Override
     public boolean onOptionsItemSelected (MenuItem item) {
         int id = item.getItemId();
@@ -30,5 +32,13 @@ public class MainMenuActivity extends AppCompatActivity {
     public void openUnitsActivity(View view) {
         Intent intent = new Intent(this, UnitConverter.class);
         startActivity(intent);
+    }
+
+    public void testMethod(MenuItem item) {
+        Context context = getApplicationContext();
+        CharSequence text = "EUREKA!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
--- app/src/main/java/com/gmail/krawiec/teodor/linkapp/UnitConverter.java
+++ app/src/main/java/com/gmail/krawiec/teodor/linkapp/UnitConverter.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
