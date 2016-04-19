0b23133d1e4afa1b18952d2a063bfa72c8908673
Theo85 <teodor.krawiec@gmail.com>
2016-04-19 17:02:30 +0200

    Android resources - knowledge consolidation

--- app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
+++ app/src/main/java/com/gmail/krawiec/teodor/linkapp/MainMenuActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    int x=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,23 @@ public class MainMenuActivity extends AppCompatActivity {
         int id = item.getItemId();
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onBackPressed(){
+
+        if (x==0){
+            x++;
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, R.string.exit_messege, duration);
+            toast.show();
+        }else{
+            System.exit(0);
+        }
+
+        Log.d("my test","onBackPressed Called");
+    }
+
 
     public void openUnitsActivity(View view) {
         Intent intent = new Intent(this, UnitConverter.class);
